994a61e52c6d8735a7be7700620ad3439171e11c
Russell Wustenberg <rw2873@nyu.edu>
2021-12-06 14:32:34 -0500

    Initial commit.

---
+++ main.py
@@ -0,0 +1,11 @@
+''''
+The goal of this repository is to give a basic implementation of the Log-Euclidean Polyrigid Image
+Registration algorithm as outlined in Arsigny, et al., in the following article:
+
+Vincent Arsigny, Olivier Commowick, Nicholas Ayache, Xavier Pennec. A Fast and Log-Euclidean
+Polyaﬀine Framework for Locally Linear Registration. Journal of Mathematical Imaging and Vision,
+Springer Verlag, 2009, 33 (2), pp.222-238. 10.1007/s10851-008-0135-9. inria-00616084
+
+This implementation is part of Russell Wustenberg's work with the Visualization, Imaging and Data
+Analysis (VIDA) research lab at New York University's Tandon School of Engineering.
+'''

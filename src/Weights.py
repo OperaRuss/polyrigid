Corrected Commowick weight calculation.

src/Weights.py
@@ -32,9 +32,9 @@ def getCommowickWeight(componentSegmentation: np.ndarray, gamma: float):
     '''
     # Makki's implementation proposed an alternative function with steeper drop off
     # 2.0 / ( 1.0 + np.exp(0.4 * getDistanceToComponentRegion(componentSegmentation))
-    return (1.0 / (1.0 - gamma * pow(getDistanceToComponentRegion(componentSegmentation), 2)))
+    return (1.0 / (1.0 + (gamma * pow(getDistanceToComponentRegion(componentSegmentation), 2))))
 
-def getNormalizedCommowickWeight(listComponentSegmentation: list):
+def getNormalizedCommowickWeight(listComponentSegmentation: list, listParamsRateOfDecay: list, is3D: bool=False):
     '''
     Function computes the normalized contribution weight at each pixel for each component. First,
     the raw contribution is contributed using the Commowick weight function.  Then, a sum is calculated
@@ -43,15 +43,46 @@ def getNormalizedCommowickWeight(listComponentSegmentation: list):
     and care must be taken when evaluating object behavior near the border of the region of interest.
 
     :param listComponentSegmentation: a list of images, as numpy arrays, containing component segmentation labels
+    :param listParamsRateOfDecay: a list of float-type hyperparameters specifying the rate of decay of influence for each component
+    :param is3D: boolean argument for whether images are 3D (True) or 2D (False).
     :return: a list of numpy arrays containing normalized contribution weight of each component
     '''
+    numComponents = len(listComponentSegmentation)
     listCommowickWeights = []
-    for img in listComponentSegmentation:
-        listCommowickWeights.append(getCommowickWeight(img))
+    for idx in range(numComponents):
+        listCommowickWeights.append(getCommowickWeight(listComponentSegmentation[idx], listParamsRateOfDecay[idx]))
+    
+    imWidth = listComponentSegmentation[0].shape[0]
+    imHeight = listComponentSegmentation[0].shape[1]
+    if(is3D):
+        imDepth = listComponentSegmentation[0].shape[3]
+        sumImage = np.zeros((imWidth,imHeight),dtype=np.float64)
+
+        for x in range(imWidth):
+            for y in range(imHeight):
+                for z in range(imDepth):
+                    for n in range(numComponents):
+                        sumImage[x,y] += listCommowickWeights[n][x,y]
+    else:
+        sumImage = np.zeros((imWidth, imHeight), dtype=np.float64)
+
+        for x in range(imWidth):
+            for y in range(imHeight):
+                for n in range(numComponents):
+                    sumImage[x,y] += listCommowickWeights[n][x,y]
+    
+    listNormalizedWeights = []
+
+    for n in range(numComponents):
+        listNormalizedWeights.append(np.divide(listComponentSegmentation[n], sumImage))
+        utils.showNDA_InEditor_BW(listNormalizedWeights[n])
+    
+    return listNormalizedWeights
+    
 
 import Shape
 
-img = np.zeros((64,64), dtype=float)
+img = np.zeros((64,64), dtype=np.float64)
 c = Shape.Ellipse([32,32],30,10,2)
 
 for x in range(img.shape[0]):
@@ -59,10 +90,22 @@ for x in range(img.shape[0]):
         if(c.isWithin([x,y])):
             img[x,y] = 1.0
 
+img1 = np.zeros((64,64), dtype=np.float64)
+c = Shape.Ellipse([12,12],10,10,2)
+for x in range(img.shape[0]):
+    for y in range(img.shape[1]):
+        if(c.isWithin([x,y])):
+            img1[x,y] = 1.0
+
+listImages = [img, img1]
+listRatesOfDecay = [0.25, 0.1]
+
 import utilities as utils
 
-utils.showNDA_InEditor_BW(img)
+for n in range(len(listImages)):
+    utils.showNDA_InEditor_BW(listImages[n], "Image " + str(n))
 
-w = getCommowickWeight(img, 0.25)
+listNormalizedWeights = getNormalizedCommowickWeight(listImages, listRatesOfDecay, is3D=False)
 
-utils.showNDA_InEditor_BW(w)
+for n in range(len(listImages)):
+    utils.showNDA_InEditor_BW(listNormalizedWeights[n], "Normalized " + str(n) + ", Max: " + str(np.max(listNormalizedWeights[n])))
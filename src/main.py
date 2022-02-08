30538408ce1d2d30cc9bcbcbee50a42524e34d0b
OperaRuss <68122626+OperaRuss@users.noreply.github.com>
2022-02-08 13:06:21 -0500

    First Draft of PyTorch Learning Algorithm. Grad Currently Reporting as Zero.

--- src/main.py
+++ src/main.py
@@ -88,10 +88,13 @@ weightVolume = torch.reshape(weightVolume,shape=(imageWidth*imageHeight*imageDep
 
 print("Initializing Component Transforms.")
 # STEP 3: Initialize transform variables
-eye = torch.zeros((4,4), dtype=torch.float64) # zeros for [ L v ] matrix
-eye = eye.reshape((1,4,4))
-componentTransforms = eye.repeat(numComponents,1,1)
-componentTransforms = torch.autograd.Variable(data=componentTransforms,requires_grad=True)
+eye = np.zeros((numComponents,4,4), dtype=np.float64) # zeros for [ L v ] matrix
+for i in range(numComponents):
+    eye[i] = np.eye(4)
+eye = np.reshape(eye,(numComponents,16))
+eye = torch.tensor(eye, requires_grad=True)
+componentTransforms = torch.autograd.Variable(data=eye,requires_grad=True)
+componentTransforms.retain_grad() # Doesn't count as a leaf tensor?
 # Begin by initializing 8 transform matrices in a batch, but do so _already in the log domain_.
 # This means the values are speed displacements (velocity vectors) and each transform is in the
 # general 4x4 form
@@ -99,15 +102,13 @@ componentTransforms = torch.autograd.Variable(data=componentTransforms,requires_
 #       [ 0 0 ]
 # As defined in 2009, Arsigny, Fast Log-Euclidean Transforms.
 
-componentTransforms = torch.reshape(componentTransforms,(numComponents,16))
-# Linearize the transforms into an [N x 16] matrix for matrix multiplication.
-
 print("Entering Registration Loop.")
 # STEP 4: ENTER UPDATE LOOP
 stop_loss = 1e-5
 step_size = stop_loss / 3.0
 maxItrs = 1
 
+# Create a regular sample grid in three dimensions
 S_d = np.linspace(-1,1,dim[0])
 S_w = np.linspace(-1,1,dim[1])
 S_h = np.linspace(-1,1,dim[2])
@@ -123,32 +124,24 @@ for i in range(maxItrs):
     for i in range(imageWidth*imageHeight*imageDepth):
         LEPTImageVolume[i] = torch.matrix_exp(torch.reshape(fusedVectorLogs[i],(4,4)))
 
-    LEPTImageVolume = LEPTImageVolume.reshape(LEPTImageDimensions)
-
-
-    print(LEPTImageVolume[0,0,0])
+    LEPTImageVolume = LEPTImageVolume.reshape((20,64,64,4,4))
 
     print("\tCalculating Displacements...")
     displacementFieldDimensions = _augmentDimensions(imageDimensions,len(imageDimensions))
+    print(displacementFieldDimensions)
     displacementField = torch.zeros(displacementFieldDimensions,dtype=torch.float64)
 
-    normCoords = [[2.0 / dim[0], 0, 0, -((dim[0] - 1) / dim[0])],
-                  [0, 2.0 / dim[1], 0, -((dim[1] - 1) / dim[1])],
-                  [0, 0, 2.0 / dim[2], -((dim[2] - 1) / dim[2])],
-                  [0, 0, 0, 1]]
-    norm = torch.tensor(normCoords,dtype=torch.float64)
-    if(len(imageDimensions) == 3):
-        for depth in range(imageDimensions[0]):
-            print("Processing slice ",depth + 1," of ",imageDepth,".")
-            for row in range(imageDimensions[1]):
-                for col in range(imageDimensions[2]):
-                    homogeneousPoint = torch.tensor([depth,row,col,1],dtype=torch.float64)
-                    homogeneousPoint = torch.matmul(norm,homogeneousPoint)
-                    newPoint = torch.matmul(LEPTImageVolume[depth,row,col],homogeneousPoint)
-                    newPoint = torch.divide(newPoint,newPoint[len(imageDimensions)])[:len(imageDimensions)]
-                    oldPoint = homogeneousPoint[:len(imageDimensions)]
-                    trajectory = oldPoint - newPoint
-                    displacementField[depth,row,col] = oldPoint + trajectory
+
+    for depth in range(len(S_d)):
+        print("Processing slice ",depth + 1," of ",imageDepth,".")
+        for row in range(len(S_h)):
+            for col in range(len(S_w)):
+                homogeneousPoint = torch.tensor([S_d[depth],S_h[row],S_w[col],1],dtype=torch.float64)
+                newPoint = torch.matmul(LEPTImageVolume[depth,row,col],homogeneousPoint)
+                newPoint = torch.divide(newPoint,newPoint[len(imageDimensions)])[:len(imageDimensions)]
+                oldPoint = homogeneousPoint[:len(imageDimensions)]
+                displacement = oldPoint - newPoint
+                displacementField[depth,row,col] = oldPoint + displacement
 
 # STEP 5: Warp Image
 # Originally, we had discussed using SimpleITK.  There is an issue in that SimpleITK relies upon
@@ -165,16 +158,13 @@ for i in range(maxItrs):
 
 displacementField = displacementField.unsqueeze(0)
 
-print(displacementField.max(),displacementField.min())
-
 import torch.nn.functional as F
-movingImage = movingImage.unsqueeze(0).unsqueeze(0)
-warped = F.grid_sample(movingImage,displacementField,mode='bilinear',padding_mode='zeros',align_corners=False)
+movingImage = movingImage.unsqueeze(0).unsqueeze(0).permute((0,1,4,3,2)) # (Batch, Channels, Height, Width, Depth)
+warped = F.grid_sample(movingImage,displacementField,mode='nearest',padding_mode='zeros',align_corners=False)
 
 print("Calculating loss...")
 loss = F.mse_loss(warped.squeeze(),fixedImage)
 print("Loss: ",loss)
-print(componentTransforms.requires_grad)
 
 loss.backward()
 print("Component Gradient: ",componentTransforms.grad)
